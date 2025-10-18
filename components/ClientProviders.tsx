6a1dd6e2a53a3abbd424fd74e91ba3bc6770d4c9
MCoder-Desenv <mcoderdesenv@gmail.com>
2025-10-17 23:49:18 -0300

    0 providers

--- components/ClientProviders.tsx
+++
@@ -1,7 +0,0 @@
-// 'use client';
-// import React from 'react';
-// import { AuthProvider } from '@/context/AuthContext';
-
-// export default function ClientProviders({ children, companyId }: { children: React.ReactNode; companyId?: string }) {
-//   return <AuthProvider companyId={companyId}>{children}</AuthProvider>;
-// }
